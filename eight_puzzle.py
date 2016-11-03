844de5af91dc11fd5cca2bff52afc322adf0ad35
xythyl <mlauh001@ucr.edu>
2016-11-03 16:36:05 -0700

    added misplaced tile and manhattan distance functions

--- eight_puzzle.py
+++ eight_puzzle.py
@@ -18,9 +18,44 @@ def general_search(problem, queueing_function):
   #end
 '''
 default = [[1,8,2],[0,4,3],[7,6,5]]
+solution = [[1,2,3],[4,5,6],[7,8,0]]
+
+class Node:
+  def __init__(self, state, parent=None):
+    self.STATE = state
+    self.PARENT = parent
+    if parent is None:
+      self.DEPTH = 0
+    else:
+      self.DEPTH = self.PARENT.DEPTH+1
+
+  def __getitem__(self, i):
+    return self.STATE[index]
+
+def misplaced_tiles_d(state): #datatype for state is a list
+  misplaced_tiles = 0
+  for i in range(len(state)):
+    for j in range(len(state[i])):
+      if state[i][j] != solution[i][j]:
+        misplaced_tiles += 1
+  return misplaced_tiles
+
+def manhattan_d(state):
+  number_of_moves = 0
+  for i in range(len(state)):
+    for j in range(len(state[i])):
+      if state[i][j] != 0:
+        #i_goal = (state[i][j]-1)//3
+        #j_goal = (state[i][j]-1)%3
+        number_of_moves += (abs(i-((state[i][j]-1)//3)) + abs(j-((state[i][j]-1)%3)))
+  return number_of_moves
+
+#distance += abs(x_value - x_goal) + abs(y_value - y_goal)
+
 
 #converts a 2d list into a 1d list
-def conv_2d_list(old_list):   new_list = []
+def conv_2d_list(old_list):   
+  new_list = []
   for i in range(len(old_list)):
     for j in range(len(old_list[i])):
       new_list.append(old_list[i][j])
@@ -33,14 +68,12 @@ http://www.geeksforgeeks.org/check-instance-8-puzzle-solvable/
 def check_solvable(state):
   inversion_count = 0
   checked_list = conv_2d_list(state)
-  print ("Checked list")
   checked_list.remove(0)
-  print (checked_list)
   for i in range(0, len(checked_list)):
     for j in range(i, len(checked_list)):
       if checked_list[i] > checked_list[j]:
         inversion_count += 1
-  print('Inversion count is: ' + str(inversion_count))
+  #print('Inversion count is: ' + str(inversion_count))
   return not(inversion_count % 2)
 
 def get_puzzle():
@@ -70,7 +103,7 @@ def get_puzzle():
   return puzzle
 
 def puzzle_prompt():
-  text = input("Type \"1\" to use default puzzle or \"2\" to enter a puzzle: ")
+  text = input("Type \"1\" to use a default puzzle or \"2\" to enter your own puzzle: ")
   if len(text) > 1:
     print ("Error input is not valid")
     return puzzle_prompt()
@@ -84,13 +117,28 @@ def puzzle_prompt():
     print ("Error: input is not valid")
     return puzzle_prompt()
 
+def select_algorithm():
+  print ("Enter you choice of algorithm")
+  print ("1. Uniform Cost Search")
+  print ("2. A* with the Misplaced Tile heuristic")
+  print ("3. A* with the Manhattan distance heuristic")
+  selection = input()
+  if selection not in ['1','2','3']:
+    print ("Error: incorrect number selected\n")
+    return select_algorithm()
+  else:
+    return selection
+
 def main():
   print ("Eight Puzzle Solver")
   puzzle = puzzle_prompt()
-  print(puzzle)
-  print(puzzle[0])
-  print(puzzle[1])
-  print(puzzle[2])
+  select_algorithm()
+  #print(puzzle)
+  #print(puzzle[0])
+  #print(puzzle[1])
+  #print(puzzle[2])
+  print(misplaced_tiles_d(puzzle))
+  print(manhattan_d(puzzle))
   if check_solvable(puzzle):
     print("puzzle is solvable")
   else:
